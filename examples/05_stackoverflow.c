7493388677fa6c4c65ce26824586ec7285c25059
Christopher Milan <chrismilan@ucla.edu>
2025-08-16 18:53:25 -0700

    actually stackoverflow with -O2

--- examples/05_stackoverflow.c
+++ examples/05_stackoverflow.c
@@ -5,14 +5,15 @@
 
 #include <braid.h>
 
-#pragma GCC diagnostic push
 #pragma GCC diagnostic ignored "-Winfinite-recursion"
-void fn(void) { fn(); }
-#pragma GCC diagnostic pop
+int fn(void) {
+  volatile char buf[1024];
+  return fn() + buf[0];
+}
 
 int main(void) {
   braid_t b = braidinit();
-  braidadd(b, fn, 8196, "fn", CORD_NORMAL, 0);
+  braidadd(b, (void (*)(void))fn, 8196, "fn", CORD_NORMAL, 0);
   braidstart(b);
   return 0;
 }
